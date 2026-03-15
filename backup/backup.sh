1e497925af9e80284116049a6ee30a88577bd097
Federico Leoni <151515138+FedericoLeoni@users.noreply.github.com>
2026-03-15 18:08:00 +0100

    backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,22 +2,17 @@
 
 DATA=$(date +%F_%H-%M)
 
-# backup destination
 DEST1="backupuser@192.168.1.xxx:/mnt/pool1/backup/db"
 DEST2="backupuser@192.168.1.xxx:/mnt/pool1/backup/docker"
 
-# enter in EspoCRM container instance and dump DB
 docker exec crm-db \
   mysqldump -u root -pxxxxx espocrm > /tmp/db_$DATA.sql
 
-# backup docker compose
 tar czf /tmp/docker_$DATA.tar.gz /root/docker-lab/docker
 
-# Sync on destination NAS
 rsync -avz /tmp/db_$DATA.sql $DEST1/
 rsync -avz /tmp/docker_$DATA.tar.gz $DEST2/
 
-# mantain the last 3 files
 ssh backupuser@192.168.1.xxx "
   cd /mnt/pool1/backup/db &&
   ls -1t db_*.sql | tail -n +4 | xargs -r rm --
@@ -28,7 +23,6 @@ ssh backupuser@192.168.1.xxx "
    ls -1t docker_*.tar.gz | tail -n +4 | xargs -r rm --
 "
 
-# local cleanup
 rm /tmp/db_$DATA.sql
 rm /tmp/docker_$DATA.tar.gz
 
